6d5783d4397ef937b81d4c86c027fa347942b049
benravago <ben.ravago@aol.com>
2018-08-18 19:48:15 -0400

    indents

--- cc/leveldbjni.cc
+++ cc/leveldbjni.cc
@@ -7,27 +7,27 @@
 
 jint
 throwException(JNIEnv* env, leveldb::Status status) {
-    const char* exceptionClass = "java/lang/Exception";
+  const char* exceptionClass = "java/lang/Exception";
 
-    jclass clazz = env->FindClass(exceptionClass);
-    if (!clazz) {
-        log("Can't find exception class %s", exceptionClass);
-        return -1;
-    }
-    return env->ThrowNew(clazz, status.ToString().c_str());
+  jclass clazz = env->FindClass(exceptionClass);
+  if (!clazz) {
+    log("Can't find exception class %s", exceptionClass);
+    return -1;
+  }
+  return env->ThrowNew(clazz, status.ToString().c_str());
 }
 
 void
 log(const char *fmt, ...) {
-    int fd = open("/dev/console", O_WRONLY);
-    if (fd < 0) return;
+  int fd = open("/dev/console", O_WRONLY);
+  if (fd < 0) return;
 
-    char buffer[1000];
-    va_list ap;
-    va_start(ap, fmt);
-    vsprintf(buffer, fmt, ap);
-    va_end(ap);
+  char buffer[1000];
+  va_list ap;
+  va_start(ap, fmt);
+  vsprintf(buffer, fmt, ap);
+  va_end(ap);
 
-    write(fd, buffer, strlen(buffer));
-    close(fd);
+  write(fd, buffer, strlen(buffer));
+  close(fd);
 }
